5e97889bdfe8a82c545b241b5736c0f3b9f0f63e
Dipesh Sukhani <me@dipeshsukhani.dev>
2020-12-10 15:49:05 +0800

    Updating the code for assignment 1

--- routes.js
+++ routes.js
@@ -7,34 +7,45 @@ const requestHandler = (req, res) => {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/create-user" method="POST"><input type="text" placeholder="insert username" name="username"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
   }
   
-  if (url === '/message' && method === 'POST') {
+  if (url === '/create-user' && method === 'POST') {
     const body = [];
+    let username;
     req.on('data', chunk => {
       console.log(chunk);
       body.push(chunk);
     });
-    return req.on('end', () => {
+    req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
+      username = parsedBody.split('=')[1];
+      
+      // fs.writeFile('message.txt', message, err => {
+      //   res.statusCode = 302;
+      //   res.setHeader('Location', '/');
+      //   return res.end();
+      // });
+      res.statusCode = 302;
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title></head>');
+      res.write(`<p>${username}</p>`);
+      res.write('</html>');
+
+      return res.end();
     });
+    
+    
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('<html>');
+  // res.write('<head><title>My First Page</title><head>');
+  // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  // res.write('</html>');
+  // res.end();
 };
 
 // module.exports = requestHandler;
